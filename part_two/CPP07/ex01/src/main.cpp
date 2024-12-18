b4931699220849723e415c6c4e6e7ee8e37eb83a
Louise Leciak <lleciak@made-f0Cr13s5.clusters.42paris.fr>
2024-12-18 11:01:57 +0100

    ADD: EX01(CPP07):

---
+++ part_two/CPP07/ex01/src/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lleciak <lleciak@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 13:24:41 by lleciak           #+#    #+#             */
+/*   Updated: 2024/12/18 10:50:18 by lleciak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "../inc/iter.hpp"
+
+void	print(const std::string &str){
+	std::cout << str << std::endl;
+}
+
+void	printInt(const int &i){
+	std::cout << i << std::endl;
+}
+
+int main()
+{
+	std::string tab[] = {"youpi", "coucou", "hihi", "bip", "boup"};
+	int intTab[] = {1, 2, 3, 4, 5};
+	
+	iter(tab,5,print);
+	iter(intTab,5,printInt);
+}
