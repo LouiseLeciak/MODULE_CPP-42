20adeffa32a5144c679299165d8dcad24eb72d39
Louise Leciak <lleciak@paul-f5Br6s1.clusters.42paris.fr>
2025-03-21 08:16:07 +0100

    FIX: BETTER MAIN(CPP08EX00):

--- part_two/CPP08/ex00/main.cpp
+++ part_two/CPP08/ex00/main.cpp
@@ -6,21 +6,39 @@
 /*   By: lleciak <lleciak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 12:48:38 by lleciak           #+#    #+#             */
-/*   Updated: 2024/12/19 08:20:28 by lleciak          ###   ########.fr       */
+/*   Updated: 2025/03/21 08:15:21 by lleciak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/easyfind.hpp"
+#include "easyfind.hpp"
 #include <vector>
+#include <list>
 
 int main(void)
 {
+    std::cout << "*** VECTOR ***\n" << std::endl;
     try
     {
         std::vector<int>tab;     
         
         tab.push_back(1);
-        tab.push_back(2);
+        tab.push_back(7);
+        tab.push_back(42);
+        tab.push_back(3);
+        easyfind(tab, 42);
+        easyfind(tab, 9);
+    }
+    catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+    std::cout << "\n\n*** LIST ***\n" << std::endl;
+    try
+    {
+        std::list<int>tab;     
+        
+        tab.push_back(1);
+        tab.push_back(7);
         tab.push_back(42);
         tab.push_back(3);
         easyfind(tab, 42);
